c8e412289a67a416995fc7edd8a139de7b68678f
carlosRosa <crafaelrosa@gmail.com>
2025-10-04 16:58:19 -0300

    ajustando encoding

--- hr-api-gateway-clound/src/main/java/hr_api_gateway_zuul/HrApiGatewayZuulApplication.java
+++ hr-api-gateway-clound/src/main/java/hr_api_gateway_zuul/HrApiGatewayZuulApplication.java
@@ -2,8 +2,10 @@ package hr_api_gateway_zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class HrApiGatewayZuulApplication {
 
 	public static void main(String[] args) {
